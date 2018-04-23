2738ac08d235d7742c65b3894ba5593e79841b59
Brightcells <brightcells@gmail.com>
2018-04-23 15:34:40 +0800

    :art: Realize reply image/voice/video/music/news

--- pywe_message_reply/__init__.py
+++ pywe_message_reply/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from pywe_message_reply.pymsgreply import Reply, reply_text
+from pywe_message_reply.pymsgreply import Reply, reply_text, reply_image, reply_voice, reply_video, reply_music, reply_news
--- pywe_message_reply/pymsgreply.py
+++ pywe_message_reply/pymsgreply.py
@@ -25,7 +25,7 @@ class Reply(object):
             ('Content', content)
         ]))
 
-    def image(self):
+    def image(self, touser=None, fromuser=None, media_id=None):
         """
         <xml>
             <ToUserName>< ![CDATA[toUser] ]></ToUserName>
@@ -37,8 +37,14 @@ class Reply(object):
             </Image>
         </xml>
         """
+        return '<xml><ToUserName><![CDATA[{toUser}]]></ToUserName><FromUserName><![CDATA[{fromUser}]]></FromUserName><CreateTime>{createTime}</CreateTime><MsgType><![CDATA[image]]></MsgType><Image><MediaId><![CDATA[{media_id}]]></MediaId></Image></xml>'.format(
+            toUser=touser,
+            fromUser=fromuser,
+            createTime=tc.utc_timestamp(),
+            media_id=media_id,
+        )
 
-    def voice(self):
+    def voice(self, touser=None, fromuser=None, media_id=None):
         """
         <xml>
             <ToUserName>< ![CDATA[toUser] ]></ToUserName>
@@ -50,8 +56,14 @@ class Reply(object):
             </Voice>
         </xml>
         """
+        return '<xml><ToUserName><![CDATA[{toUser}]]></ToUserName><FromUserName><![CDATA[{fromUser}]]></FromUserName><CreateTime>{createTime}</CreateTime><MsgType><![CDATA[voice]]></MsgType><Voice><MediaId><![CDATA[{media_id}]]></MediaId></Voice></xml>'.format(
+            toUser=touser,
+            fromUser=fromuser,
+            createTime=tc.utc_timestamp(),
+            media_id=media_id,
+        )
 
-    def video(self):
+    def video(self, touser=None, fromuser=None, media_id=None, title=None, description=None):
         """
         <xml>
             <ToUserName>< ![CDATA[toUser] ]></ToUserName>
@@ -65,8 +77,16 @@ class Reply(object):
             </Video>
         </xml>
         """
+        return '<xml><ToUserName>< ![CDATA[{toUser}]]></ToUserName><FromUserName><![CDATA[{fromUser}]]></FromUserName><CreateTime>{createTime}</CreateTime><MsgType><![CDATA[video]]></MsgType><Video><MediaId><![CDATA[{media_id}]]></MediaId><Title><![CDATA[{title}]]></Title><Description><![CDATA[{description}]]></Description></Video></xml>'.format(
+            toUser=touser,
+            fromUser=fromuser,
+            createTime=tc.utc_timestamp(),
+            media_id=media_id,
+            title=title,
+            description=description,
+        )
 
-    def music(self):
+    def music(self, touser=None, fromuser=None, title=None, description=None, music_url=None, hq_music_url=None, media_id=None):
         """
         <xml>
             <ToUserName>< ![CDATA[toUser] ]></ToUserName>
@@ -82,8 +102,18 @@ class Reply(object):
             </Music>
         </xml>
         """
+        return '<xml><ToUserName><![CDATA[toUser]]></ToUserName><FromUserName><![CDATA[fromUser]]></FromUserName><CreateTime>{createTime}</CreateTime><MsgType><![CDATA[music]]></MsgType><Music><Title><![CDATA[{title}]]></Title><Description><![CDATA[{description}]]></Description><MusicUrl><![CDATA[{music_url}]]></MusicUrl><HQMusicUrl><![CDATA[{hq_music_url}]]></HQMusicUrl><ThumbMediaId><![CDATA[{media_id}]]></ThumbMediaId></Music></xml>'.format(
+            toUser=touser,
+            fromUser=fromuser,
+            createTime=tc.utc_timestamp(),
+            title=title,
+            description=description,
+            music_url=music_url,
+            hq_music_url=hq_music_url,
+            media_id=media_id,
+        )
 
-    def news(self):
+    def news(self, touser=None, fromuser=None, items=[]):
         """
         <xml>
             <ToUserName>< ![CDATA[toUser] ]></ToUserName>
@@ -107,7 +137,30 @@ class Reply(object):
             </Articles>
         </xml>
         """
+        articleCount = len(items)
+        if not 0 < articleCount < 8:
+            return
+        articleItems = ''
+        for item in items:
+            articleItems += '<item><Title><![CDATA[{title}]]></Title><Description><![CDATA[{description}]]></Description><PicUrl><![CDATA[{picurl}]]></PicUrl><Url><![CDATA[{url}]]></Url></item>'.format(
+                title=item.get('title', ''),
+                description=item.get('description', ''),
+                picurl=item.get('picurl', ''),
+                url=item.get('url', ''),
+            )
+        return '<xml><ToUserName><![CDATA[{toUser}]]></ToUserName><FromUserName><![CDATA[{fromUser}]]></FromUserName><CreateTime>{createTime}</CreateTime><MsgType><![CDATA[news]]></MsgType><ArticleCount>{articleCount}</ArticleCount><Articles>{articleItems}</Articles></xml>'.format(
+            toUser=touser,
+            fromUser=fromuser,
+            createTime=tc.utc_timestamp(),
+            articleCount=articleCount,
+            articleItems=articleItems,
+        )
 
 
 reply = Reply()
 reply_text = reply.text
+reply_image = reply.image
+reply_voice = reply.voice
+reply_video = reply.video
+reply_music = reply.music
+reply_news = reply.news
